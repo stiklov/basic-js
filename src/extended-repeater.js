5ea218b49a1f59e25a9180eaa900b62de5031052
Димка Стиклов <dimkastiklov@Dimkas-MacBook-Pro.local>
2023-12-23 03:53:00 +0400

    feat: done extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let separator = '+';
+  if (options.separator !== undefined) separator = String(options.separator);
+  let additionSeparator = '|';
+  if (options.additionSeparator !== undefined) additionSeparator = String(options.additionSeparator);
+  let addition = '';
+  if (options.addition !== undefined) addition = String(options.addition);
+  let repeatTimes = options.repeatTimes || 1;
+  let additionRepeatTimes = options.additionRepeatTimes || 1;
+
+  str = (str + (addition + additionSeparator).repeat(additionRepeatTimes)).slice(0, -additionSeparator.length);
+  str = (str + separator).repeat(repeatTimes).slice(0, -separator.length);
+  return str;
 }
 
 module.exports = {

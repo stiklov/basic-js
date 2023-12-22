372f063e2eb40cd230ce5b5eccdbed5daf0b9e59
Димка Стиклов <dimkastiklov@Dimkas-MacBook-Pro.local>
2023-12-22 04:32:03 +0400

    feat: done count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,8 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix) {
+  return matrix.map(row => row.filter(x => x == '^^').length).reduce((x, sum) => sum + x, 0);
 }
 
 module.exports = {
